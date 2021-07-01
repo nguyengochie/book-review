5c6cdec2cb18b78eba011c174fb952419a2957d5
Hieu <nguyengoc.hie@gmail.com>
2021-07-02 00:55:15 +0700

    feat: UI list books

--- src/Component/HomeContent/HomeContent.style.tsx
+++ src/Component/HomeContent/HomeContent.style.tsx
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, fade } from "@material-ui/core/styles";
 
 const URL_IMAGE = "https://demo.joomlashine.com/joomla-templates/jsn_bookstore/pro/novelist/images/joomlashine/sample/homepage/photo-1518306965192-44da60abb26f%201.png";
 
@@ -6,14 +6,16 @@ export const useStyles = makeStyles(
   (theme) => ({
     root: {},
     firstContent: {
-      background: `url(${URL_IMAGE}) no-repeat center center fixed`,
-      backgroundSize: "contain",
+      background: `url(${URL_IMAGE}) no-repeat`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
       width: "100vw",
       height: 590,
       textAlign: "center",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
+      marginTop: 40
     },
     mainContentCard: {
       background: "#dda96d99",
@@ -24,10 +26,121 @@ export const useStyles = makeStyles(
       flexDirection: "column",
       justifyContent: "center",
       color: "#ffff",
+      padding: "30px 60px",
     },
     txtTitle: {
-      fontSize: 40,
-      color: "#ffff"
+      fontSize: 28,
+      color: "#ffff",
+      marginBottom: 20
+    },
+    search: {
+      marginTop: 45,
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+    txtContent: {
+      display: "flex",
+      alignItems: "center",
+      padding: "1rem 0px",
+      position: "relative",
+      width: "80vw",
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+    txtTop10: {
+      "&::before": {
+        content: '""',
+        width: "100%",
+        borderTop: "1px solid #c5c5c5",
+        marginRight: 10
+      },
+      "&::after": {
+        content: '""',
+        width: "100%",
+        borderTop: "1px solid #c5c5c5",
+        marginLeft: 10
+      },
+      alignItems: "center",
+      display: "flex",
+      width: "100%",
+      whiteSpace: "nowrap",
+      fontSize: 20,
+      fontWeight: 600,
+    },
+    listBooksContent: {
+      marginTop: 20,
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    listBook: {
+      maxWidth: "80vw",
+      marginTop: 15
+    },
+    cardContent: {
+      background: "#f5f5f5",
+      height: 437,
+      width: 262,
+      "&:hover": {
+        boxShadow: "0px 0px 7px 1px #c5c5c5",
+      },
+      cursor: "pointer",
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+    imgCard: {
+      display: "flex",
+      width: "100%",
+      height: "73%",
+      overflow: "hidden"
+    },
+    descCard: {
+      height: "27%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    txtBookTitle: {
+      fontSize: 17
+    },
+    txtPrice: {
+      fontSize: 14,
+      marginBottom: 10
     }
   }),
   { index: 1 }
--- src/Component/HomeContent/HomeContent.tsx
+++ src/Component/HomeContent/HomeContent.tsx
@@ -1,9 +1,38 @@
-import { Typography } from '@material-ui/core';
 import React from 'react';
+import { Typography, InputBase, Grid } from '@material-ui/core';
 import { useStyles } from './HomeContent.style';
+import SearchIcon from '@material-ui/icons/Search';
+import Rating from '@material-ui/lab/Rating';
+import product1 from "../../assets/books/product1.jpeg";
+import product2 from "../../assets/books/product2.jpeg";
+import product3 from "../../assets/books/product3.jpeg";
+import product4 from "../../assets/books/product4.jpeg";
 
 const HomeContent = () => {
   const classes = useStyles();
+
+  const renderBooks = () => {
+    return Array.from(Array(5)).map(() => {
+      return [product1, product2, product3, product4].map((product: any) => {
+        return (<Grid item xs={3}>
+          <div className={classes.cardContent}>
+            <div className={classes.imgCard}>
+              <img src={product} alt="" />
+            </div>
+            <div className={classes.descCard}>
+              <Typography className={classes.txtBookTitle}>
+                Blowaway
+              </Typography>
+              <Typography className={classes.txtPrice}>
+                29,000VND
+              </Typography>
+              <Rating defaultValue={3} />
+            </div>
+          </div>
+        </Grid>)
+      })
+    })
+  }
 
   return (
     <div className={classes.root}>
@@ -14,7 +43,28 @@ const HomeContent = () => {
             We stock more than 20 million titles for free delivery worldwide to over 170 countries.
             Here's where you can find an enjoyable shopping experience.
           </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
         </div>
+      </div>
+      <div className={classes.listBooksContent}>
+        <div className={classes.txtContent}>
+          <Typography className={classes.txtTop10}>TOP GOODBOOKS</Typography>
+        </div>
+        <Grid className={classes.listBook} container spacing={4}>
+          {renderBooks()}
+        </Grid>
       </div>
     </div>
   );
